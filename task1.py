8a92f0bd15beb4ecf3cf0ed835a5e7dd53e553b5
KhushiGulati100 <queenkhushi16@gmail.com>
2026-01-03 18:02:04 +0000

    task

---
+++ task1.py
@@ -0,0 +1,7 @@
+a = int(input("enter any number: "))
+b = int(input("enter any number: "))
+print(f"addition: {a+b}")
+print(f"addition: {a-b}")
+print(f"addition: {a*b}")
+print(f"addition: {a/b}")
+
