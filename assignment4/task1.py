d769d86e1a01b2752018665ea91fc6f1b1718280
KhushiGulati100 <queenkhushi16@gmail.com>
2026-01-09 08:55:51 +0000

    task1

--- assignment4/task1.py
+++ assignment4/task1.py
@@ -1,10 +1,12 @@
 try:
-    with open("sample.txt", "r") as f:
+    with open("assignment4/sample.txt", "r") as f:
         list = f.readlines()
         print("Reading the contents of the file: ")
-        for index,i in list:
+        for index,i in enumerate(list):
             index+=1
-            print(f"line{index}: {i}")
+            print(f"line{index}: {i.strip()}")
         print("We have reached the end of file.")
 except FileNotFoundError:
     print("The file 'sample.txt' was not found.")
+
+    
